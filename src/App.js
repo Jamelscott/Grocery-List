c0b6e9c071414a7212a34eb3a071dac421b2687e
Jamel-bbox <jamel@balloonbox.io>
2022-06-13 16:10:25 -0700

    basic functionality accomplished, bar the total counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Item from "./Item/Item";
 
 function App() {
+  const [currentItem, setCurrentItem] = useState("");
+  const [items, setItems] = useState([]);
+  const allItems = items.map((elem, idx) => {
+    return (
+      <Item
+        setItems={setItems}
+        key={`item-${idx + 1}`}
+        index={idx + 1}
+        name={elem}
+      />
+    );
+  });
+
+  const handleAddItem = (e) => {
+    e.preventDefault();
+    setItems([...items, currentItem]);
+    setCurrentItem("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="list-container">
+      <form onSubmit={handleAddItem}>
+        <input
+          type="text"
+          placeholder="add an item .."
+          value={currentItem}
+          onChange={(e) => setCurrentItem(e.target.value)}
+        />
+        <input type="submit" value="+" />
+      </form>
+      {allItems}
     </div>
   );
 }
