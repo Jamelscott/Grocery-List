3c6fca150233c07a84b3cc1cd72beef2763c86b9
Jamel-bbox <jamel@balloonbox.io>
2022-06-13 23:26:11 -0700

    all features and styles complete bar attaching a server/database and a custom checkbox

--- src/Item/Item.jsx
+++ src/Item/Item.jsx
@@ -1,6 +1,6 @@
 import "./item.css";
 import { useState } from "react";
-function Item({ index, name }) {
+function Item({ index, name, setTotal, total, items, setItems }) {
   const [count, setCount] = useState(1);
   const [complete, setComplete] = useState(false);
   const handleComplete = () => {
@@ -10,27 +10,58 @@ function Item({ index, name }) {
   };
 
   const handleCounterDown = () => {
-    if (count < 1) {
+    if (count < 2) {
       return;
     }
     setCount(count - 1);
+    setTotal(total - 1);
+  };
+
+  const handleCounterUp = () => {
+    setCount(count + 1);
+    setTotal(total + 1);
+  };
+  const handleRemoveItem = () => {
+    console.log(items);
+    let newList = items.filter((item) => {
+      return item != name;
+    });
+    setItems(newList);
+    setTotal(total - count);
   };
   return (
-    <div className="item-container">
-      <div className="counter-info">
-        <input type="checkbox" onClick={handleComplete}></input>
-        <p id={`itemName${index}`}> {name}</p>
+    <>
+      <div className="item-container">
+        <div className="counter-info">
+          <input
+            className="item-checkbox"
+            type="checkbox"
+            onClick={handleComplete}
+          ></input>
+          <p className="counter-name" id={`itemName${index}`}>
+            {" "}
+            {name}
+          </p>
+        </div>
+        <div className="counter-container">
+          <button
+            className="counter-button counter-button-right"
+            onClick={handleCounterDown}
+          >
+            {"<"}
+          </button>
+          <p className="counter-number">{count}</p>
+          <button className="counter-button" onClick={handleCounterUp}>
+            {">"}
+          </button>
+          <p>{""}</p>
+          <button className="counter-remove" onClick={handleRemoveItem}>
+            {""}x
+          </button>
+        </div>
       </div>
-      <div className="counter-container">
-        <button className="counter-button" onClick={() => setCount(count + 1)}>
-          +
-        </button>
-        <p>{count}</p>
-        <button className="counter-button" onClick={handleCounterDown}>
-          -
-        </button>
-      </div>
-    </div>
+      <hr></hr>
+    </>
   );
 }
 
